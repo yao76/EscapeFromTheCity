f9128a51b283a589ee6af1ee2cb88c34237ddcb4
jhoney92021 <51093828+jhoney92021@users.noreply.github.com>
2019-08-23 07:56:19 -0700

    Added Options

--- app/src/main/java/com/citygames/escapefromthecity/MainActivity.java
+++ app/src/main/java/com/citygames/escapefromthecity/MainActivity.java
@@ -8,7 +8,9 @@ import android.widget.Button;//FOR BUTTONS
 import android.widget.TextView;//FOR TEXT VIEW
 
 public class MainActivity extends AppCompatActivity {
-    Button Start;
+    Button Start,
+            Option_1,
+            Option_2;
 
     TextView Splash;
 
@@ -17,22 +19,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //PLAYER
-            final Player thisPlayer = new Player();
+        final Player thisPlayer = new Player();
         //PLAYER
         //START TO PLAYER SPLASH
-            Start = findViewById(R.id.ViewStart);
-            Start.setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View v)
-                    {
-                        setContentView(R.layout.player_status);
-                    }
-                }
-            );
+        Start = findViewById(R.id.ViewStart);
+        Start.setOnClickListener
+                (new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                         setContentView(R.layout.player_status);
+                     }
+                 }
+                );
         //START TO PLAYER SPLASH
         //Scenario 0 to Next Scenario
-
+        Option_1 = findViewById(R.id.option_go_1);
+        if (Option_1 != null) {
+            Option_1.setOnClickListener
+                    (new View.OnClickListener() {
+                         @Override
+                         public void onClick(View v) {
+                             setContentView(R.layout.option_1);
+                         }
+                     }
+                    );
+        }
+        Option_2 = findViewById(R.id.option_go_2);
+        if (Option_2 != null)
+        {
+            Option_2.setOnClickListener
+                    (new View.OnClickListener() {
+                         @Override
+                         public void onClick(View v) {
+                             setContentView(R.layout.option_2);
+                         }
+                     }
+                    );
+        }
         //Scenario 0 to Next Scenario
     }
 }

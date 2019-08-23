869fc3cdbf03f45d30c95eab83d634792bcd871d
jhoney92021 <51093828+jhoney92021@users.noreply.github.com>
2019-08-22 17:48:10 -0700

    Added More Views

--- app/src/main/java/com/citygames/escapefromthecity/MainActivity.java
+++ app/src/main/java/com/citygames/escapefromthecity/MainActivity.java
@@ -17,22 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //PLAYER
-        final Player thisPlayer = new Player();
+            final Player thisPlayer = new Player();
         //PLAYER
         //START TO PLAYER SPLASH
-        Start = findViewById(R.id.ViewStart);
-        Start.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
+            Start = findViewById(R.id.ViewStart);
+            Start.setOnClickListener(new View.OnClickListener()
                 {
-                    setContentView(R.layout.player_status);
-                    Splash = findViewById(R.id.ViewSplash);
-                    String toAdd = thisPlayer.Name + thisPlayer.Determination + thisPlayer.Intelligence;
-                    Splash.setText(toAdd);
+                    @Override
+                    public void onClick(View v)
+                    {
+                        setContentView(R.layout.player_status);
+                    }
                 }
-            }
-        );
+            );
         //START TO PLAYER SPLASH
+        //Scenario 0 to Next Scenario
+
+        //Scenario 0 to Next Scenario
     }
 }
